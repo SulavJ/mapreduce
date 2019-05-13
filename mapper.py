d030cf49172ec8e99ec3dd5ed766573c26196ce9
SulavJ <jha.sulav@gmail.com>
2019-05-13 21:21:15 +0200

    output for 5th

--- mapper.py
+++ mapper.py
@@ -44,7 +44,7 @@ for line in sys.stdin:
     # Key is the payment, value is the sales     
     # With a tab (\t) between key and value
     # New line \n means new record
-    if category in ['Computers', 'Cameras', 'Video Games']:
-	sys.stdout.write("{0}\t{1}\n".format(category, sales))
-    else:
-	pass
+    #if category in ['Computers', 'Cameras', 'Video Games']:
+    sys.stdout.write("{0}\t{1}\n".format(category, sales))
+    #else:
+    #pass
